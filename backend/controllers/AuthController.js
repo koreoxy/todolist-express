f1f31890125350e1e07faaa9c0fa99f020eb190e
koreoxy <rayssankn@gmail.com>
2023-11-07 21:53:48 +0700

    add login functional

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,6 +1,7 @@
 import User from '../models/UserModel.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../routes/error.js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,6 +18,29 @@ export const register = async (req, res, next) => {
     res.status(201).json('User created succesfully');
   } catch (error) {
     // next(errorHandler(550, 'error from the function'));
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong Credentials'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,83 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [dataForm, setDataForm] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setDataForm({
+      ...dataForm,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataForm),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+
+      // if login succesful and navigate to home page
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-bold text-center my-10">Login</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? 'Loading...' : 'Login'}
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account ?</p>
+        <Link to="/register">
+          <span className="text-blue-700">Register</span>
+        </Link>
+      </div>
+
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
